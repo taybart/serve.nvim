e3d76e852c2cf4c2c6bd4a6185990e7620950098
Taylor Bartlett <taybart@gmail.com>
2025-02-24 16:49:59 -0700

    fix pls

--- build.lua
+++ build.lua
@@ -1,6 +1,6 @@
 local winnr = vim.fn.win_getid()
 local bufnr = vim.api.nvim_win_get_buf(winnr)
-local dir = vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':p:h') .. '/../../'
+local dir = vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':p:h')
 
 vim.print('from build dir:', dir)
 vim.print('from build pwd', vim.pwd)
