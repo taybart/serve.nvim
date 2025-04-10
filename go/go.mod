3bf13bfeb74d4db7b17e527248625077212c4b0b
Taylor Bartlett <taybart@gmail.com>
2025-04-09 21:09:58 -0600

    feat: access-control-allow-origin: *

--- go/go.mod
+++ go/go.mod
@@ -5,5 +5,5 @@ go 1.24.0
 require (
 	github.com/neovim/go-client v1.2.1
 	github.com/taybart/log v1.6.4
-	github.com/taybart/rest v0.2.2
+	github.com/taybart/rest v0.2.3
 )
--- go/main.go
+++ go/main.go
@@ -12,12 +12,11 @@ import (
 	"github.com/taybart/rest/server"
 )
 
-var s http.Server
+var s *http.Server
 var serving bool
 
 func serve(v *nvim.Nvim, args []string) error {
 	if serving {
-
 		v.WriteOut(fmt.Sprintf("already serving at %s/\n", config.Server.Address))
 		return nil
 	}
@@ -27,8 +26,9 @@ func serve(v *nvim.Nvim, args []string) error {
 		config.Server.Address = args[0]
 	}
 	s = server.New(server.Config{
-		Addr: config.Server.Address,
-		Dir:  config.Server.Directory,
+		Addr:    config.Server.Address,
+		Dir:     config.Server.Directory,
+		Headers: map[string]string{"Access-Control-Allow-Origin": "*"},
 	})
 	log.Debugf("listening to %s...\n", config.Server.Address)
 	go func() {
